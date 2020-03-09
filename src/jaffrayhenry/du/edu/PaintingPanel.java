8089fe2b56aa325d1643e2060fea414f75f3e08d
hjaffray <hdjaffray@gmail.com>
2020-03-08 21:05:51 -0600

    more stuff

---
+++ src/jaffrayhenry/du/edu/PaintingPanel.java
@@ -0,0 +1,27 @@
+package jaffrayhenry.du.edu;
+
+import java.awt.Graphics;
+import java.util.ArrayList;
+
+import javax.swing.JPanel;
+
+public class PaintingPanel extends JPanel{
+
+	ArrayList<E> prims = new ArrayList<E>();
+	public PaintingPanel() {
+		
+		
+	}
+	
+	public void addPrimitive(PaintingPrimitive obj) {
+        this.prims.add(obj);
+}
+
+public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        for(PaintingPrimitive obj : prims {  // I named my ArrayList primitives -- could also use a standard for loop if you wish
+                    obj.draw(g);
+        }
+}
+
+}
---
+++ src/jaffrayhenry/du/edu/PaintingPrimitive.java
@@ -0,0 +1,23 @@
+package jaffrayhenry.du.edu;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public abstract class PaintingPrimitive {
+
+	Color c; //color variable
+	
+	public PaintingPrimitive(Color a) {
+		this.c = a;
+	}
+	
+	public final void draw(Graphics g) {
+		
+		g.setColor(this.c);
+		drawGeometry(g);
+	}
+	
+	protected abstract void drawGeometry(Graphics g);
+}
+
+
